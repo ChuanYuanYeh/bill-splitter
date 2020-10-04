6aee055ee68ea14ee1bc83ca1e685b2931deb6f0
ChuanYuanYeh <cyyeh@ucsd.edu>
2020-10-04 20:36:06 +0700

    Added server in app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config.suppress_callback_exceptions = True
+server = app.server
 
 style = {'maxWidth': '960px', 'margin': 'auto'}
 
